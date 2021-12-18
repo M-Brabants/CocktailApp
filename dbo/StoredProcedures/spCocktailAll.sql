399cda7fa8c26fbef172c0be28331baff2a62f3b
Marnix Brabants <71631732+M-Brabants@users.noreply.github.com>
2021-12-18 15:06:49 +0100

    Delete dbo directory

--- dbo/StoredProcedures/spCocktailAll.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[spCocktailAll]
-
-AS
-begin
-	select * from dbo.Cocktail;
-end
--- dbo/StoredProcedures/spCocktailSpecific.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[spCocktailSpecific]
-	@id int
-AS
-begin
-	select * from dbo.Cocktail
-	where Id = @id;
-end
